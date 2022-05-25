603612d494b83f1a82d03e5e883fd06c1c081a5e
Kai Hübner <kai.huebner@gmail.com>
2022-05-25 22:49:09 +1000

    Update rollup build

--- rollup.config.js
+++ rollup.config.js
@@ -2,17 +2,8 @@ import withSolid from 'rollup-preset-solid'
 import css from 'rollup-plugin-import-css'
 import { uglify } from 'rollup-plugin-uglify'
 
-export default withSolid([
-  {
-    input: 'src/index.tsx',
-    output: [
-      {
-        file: 'dist/esm/index.js',
-        format: 'es',
-        sourcemap: true,
-        exports: 'named',
-      },
-    ],
-    plugins: [css(), uglify()],
-  },
-])
+export default withSolid({
+  input: 'src/index.tsx',
+  printInstructions: false,
+  plugins: [css({ output: 'styles.css' }), uglify()],
+})
