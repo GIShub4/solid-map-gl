96697db7fbde2550f3d597f002d03a046a03b273
Kai Hübner <kai.huebner@gmail.com>
2022-10-06 12:15:10 +1000

    Update dynamic import

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,21 @@
 export const getLibrary = async isMapLibre => {
-  if (isMapLibre) {
-    const mapLib = await import(/* @vite-ignore */ 'maplibre-gl')
-    // @ts-ignore
-    await import(/* @vite-ignore */ 'maplibre-gl/dist/maplibre-gl.css')
-    return window['maplibregl'] || mapLib
-  } else {
-    const mapLib = await import(/* @vite-ignore */ 'mapbox-gl')
-    // @ts-ignore
-    await import(/* @vite-ignore */ 'mapbox-gl/dist/mapbox-gl.css')
-    return window['mapboxgl'] || mapLib
-  }
+  const mapLib = isMapLibre
+    ? await import(
+        // @ts-ignore
+        /* @vite-ignore */ `../../node_modules/maplibre-gl/dist/maplibre-gl.js`
+      )
+    : await import(
+        // @ts-ignore
+        /* @vite-ignore */ `../../node_modules/mapbox-gl/dist/mapbox-gl.js`
+      )
+  isMapLibre
+    ? await import(
+        // @ts-ignore
+        /* @vite-ignore */ `../../node_modules/maplibre-gl/dist/maplibre-gl.css`
+      )
+    : await import(
+        // @ts-ignore
+        /* @vite-ignore */ `../../node_modules/mapbox-gl/dist/mapbox-gl.css`
+      )
+  return window[isMapLibre ? 'maplibregl' : 'mapboxgl'] || mapLib
 }
